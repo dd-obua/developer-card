8bf1b59d2daa116ca30220e39e6be98a6f760072
Obua Denis Daniel <dd.obua@gmail.com>
2024-02-09 11:36:18 +0300

    Create developer card

--- src/index.js
+++ src/index.js
@@ -1,17 +1,82 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from '@testing-library/react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './css/styles.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+const Avatar = () => {
+  return <img className="developer-image" src="images/dev-img.webp" alt="Developer" />;
+};
+
+const Skill = (props) => {
+  return (
+    <li style={{ backgroundColor: props.backgroundColor }}>
+      {props.text}
+      <span>
+        <img src={props.emoji} alt="Level description emoji" />
+      </span>
+    </li>
+  );
+};
+
+const Details = () => {
+  return (
+    <div className="details">
+      <h2>Jan Doe</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nobis ad nostrum
+        fugit ipsam quasi ullam cumque ratione pariatur obcaecati laudantium, magnam
+        veniam eos? Accusamus officiis architecto corrupti maiores labore.
+      </p>
+      <ul className="skills">
+        <Skill
+          text="HTML+CSS"
+          emoji="./images/emojis/strong.jpeg"
+          backgroundColor="blue"
+        />
+        <Skill
+          text="JavaScipt"
+          emoji="./images/emojis/strong.jpeg"
+          backgroundColor="yellow"
+        />
+        <Skill
+          text="Web Design"
+          emoji="./images/emojis/strong.jpeg"
+          backgroundColor="yellowgreen "
+        />
+        <Skill
+          text="GitHub"
+          emoji="./images/emojis/thumbs-up.jpeg"
+          backgroundColor="maroon"
+        />
+        <Skill
+          text="React"
+          emoji="./images/emojis/strong.jpeg"
+          backgroundColor="lightblue"
+        />
+        <Skill
+          text="Svelte"
+          emoji="./images/emojis/strong.jpeg"
+          backgroundColor="crimson"
+        />
+      </ul>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <article className="card">
+      <Avatar />
+      <Details />
+    </article>
+  );
+};
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
